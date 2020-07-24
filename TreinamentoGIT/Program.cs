60fefd6043fc198636817d1df366c371554d76f7
AndreLagares <andrelagares@gmail.com>
2020-07-24 17:42:31 -0300

    Projeto iniciado, mostrando a mensagem Hello World

---
+++ TreinamentoGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreinamentoGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
